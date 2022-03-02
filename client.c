0b34e1aa5fce7771ed2dbc6e201b07eaa8936d7d
Anaji-el <93100719+Anaji-el@users.noreply.github.com>
2022-03-02 17:43:00 +0100

    modified client

--- client.c
+++ client.c
@@ -6,22 +6,39 @@
 /*   By: anaji-el <anaji-el@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 06:15:59 by anaji-el          #+#    #+#             */
-/*   Updated: 2022/02/27 06:17:13 by anaji-el         ###   ########.fr       */
+/*   Updated: 2022/03/02 16:32:56 by anaji-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
-int	main(int argc, char *argv[])
+void	send_char(int pid, char c)
 {
-   int	pid;
+	int	i;
+	int bit;
+	
+	i = 8
+	while(i++)
+	{
+		bit = (c << i & 128)
+		if( bit == 0)
+			kill(pid,SIGUSR2);
+		if(bit == 1)
+			kill(pid,SIGUSR1);
+	
+	}
+}
+void	send_string(int pid, char *str)
+{
+	int	i;
+	int	s;
 
-   if (argc != 2)
-   {
-   	printf("client: invalid arguments\n");
-   	exit(EXIT_FAILURE);
-   }
-   pid = atoi(argv[1]);
-   kill(pid, SIGUSR1);
-   return (0);
+	i = 0;
+	s = 0;
+	
+	while (str[i])
+	{
+		send_char(pid, str[i]);
+		i++;
+	}
 }
